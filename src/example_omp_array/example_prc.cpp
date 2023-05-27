89f9745b3e6eaabb9c2f177a4d16c888020b00a0
Bad Cast <lmecomposer@gmail.com>
2023-05-28 01:31:56 +0600

    example-fixed threads set

--- src/example_omp_array/example_prc.cpp
+++ src/example_omp_array/example_prc.cpp
@@ -7,19 +7,30 @@
 
 int main(void)
 {
-    constexpr std::size_t size = 6999999999;
+    constexpr std::size_t size = 5999999999 + 1; // 6 GB
     void* ma = malloc(size);
 
-//    int n;
-//    std::cin >> n;
-    omp_set_num_threads(8);
+    if (ma == nullptr)
+        throw 0;
 
+    int threads;
 
-    while(1)
+    std::cout << "set thread count: ";
+    std::cin >> threads;
+
+    omp_set_dynamic(0); // Explicitly disable dynamic teams
+    omp_set_num_threads(threads = std::max(1, std::min(32, threads)));
+    std::cout << "runned threads: " << threads << std::endl;
+    std::ldiv_t thr = std::ldiv(size, threads);
 #pragma omp parallel for
-    for (size_t x = 0; x < 8; ++x) {
-        memset(ma,-1,size);
+    for (size_t x = 0; x < threads; ++x) {
+        threads = omp_get_num_threads();
+        memset(ma + thr.quot * x, -1, thr.quot + thr.rem);
     }
+
+    int* last = static_cast<int*>(ma + size - sizeof(int) * 2);
+
+    free(ma);
 
     std::cout << "End program." << std::endl;
 }
