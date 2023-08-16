2d6a71c811f51c41f9400115ce59ca825755bfa2
badcast <lmecomposer@gmail.com>
2023-08-16 21:23:21 +0600

    optimize algorithms

--- src/10.array_manipulation/main.cpp
+++ src/10.array_manipulation/main.cpp
@@ -3,85 +3,117 @@
 #include <cstdint>
 #include <cmath>
 #include <tuple>
+#include <random>
+#include <type_traits>
 
 using namespace std;
 
-void reverse(vector<uintmax_t> &vec)
+template <typename T>
+class ComputeUnit
 {
-    for(int x = 0, y = vec.size() - 1; x < y; ++x, --y)
+    static_assert(!std::is_signed<T>::value, "T must be an unsigned type");
+
+public:
+    using type = T;
+    int max_range()
     {
-        uintmax_t tmp = vec[x];
-        vec[x] = vec[y];
-        vec[y] = tmp;
+        return sizeof(type) * 8 - 1;
     }
-}
-
-int bin_search(const vector<uintmax_t> &vec, uintmax_t value, uintmax_t &score)
-{
-    uintmax_t x = 0;
-    uintmax_t y = vec.size() - 1;
-    uintmax_t m;
-
-    while(x <= y)
+    void reverse(vector<T> &vec)
     {
-        score++;
-        m = (x + y) / 2;
-        if(vec[m] > value)
-            y = m - 1;
-        else if(vec[m] < value)
-            x = m + 1;
-        else
-            return m;
+        for(int x = 0, y = vec.size() - 1; x < y; ++x, --y)
+        {
+            T tmp = vec[x];
+            vec[x] = vec[y];
+            vec[y] = tmp;
+        }
     }
-    return -1;
-}
 
-void fill_arithmetic_progression(vector<uintmax_t> &vec)
-{
-    uintmax_t start = 1;
-    int difference_progression = 3;
-    for(uintmax_t x = 0; x < vec.size(); ++x)
+    void shuffle(vector<T> &vec)
     {
-        vec[x] = start;
-        start += difference_progression;
+        std::random_device rd;
+        std::mt19937 gen(rd());
+#pragma omp parallel for
+        for(T x = 0; x < vec.size(); ++x)
+        {
+            std::uniform_int_distribution<T> dis(x, vec.size());
+            T from = dis(gen);
+            swap(vec[x], vec[from]);
+        }
     }
-}
 
-uintmax_t compute(uint32_t ranges)
-{
-    uintmax_t test;
-    uintmax_t score = 0;
-    vector<uintmax_t> vec(ranges);
-
-    // fill
-    fill_arithmetic_progression(vec);
-
-    // reversing
-    // top to back
-    reverse(vec);
-    // back to top
-    reverse(vec);
-
-    if(ranges > 1)
+    T bin_search(const vector<T> &vec, T value, T &score)
     {
-        score += vec[vec.size() - 1];
-        test = bin_search(vec, vec[0], score);
-        test = bin_search(vec, vec[ranges / 2], score);
-        test = bin_search(vec, vec[ranges - 1], score);
+        T x = 0;
+        T y = vec.size() - 1;
+        T m;
+
+        while(x <= y)
+        {
+            score++;
+            m = (x + y) / 2;
+            if(vec[m] > value)
+                y = m - 1;
+            else if(vec[m] < value)
+                x = m + 1;
+            else
+                return m;
+        }
+        return -1;
     }
-    return score;
-}
+
+    void fill_arithmetic_progression(vector<T> &vec)
+    {
+        T start = 1;
+        int difference_progression = 3;
+        for(T x = 0; x < vec.size(); ++x)
+        {
+            vec[x] = start;
+            start += difference_progression;
+        }
+    }
+
+    T compute(uint32_t ranges)
+    {
+        T test;
+        T score = 0;
+        vector<T> vec(ranges);
+
+        // fill
+        fill_arithmetic_progression(vec);
+
+        // reversing
+        // top to back
+        reverse(vec);
+        // back to top
+        reverse(vec);
+
+        if(ranges > 1)
+        {
+            score += vec[vec.size() - 1];
+            test = bin_search(vec, vec[0], score);
+            test = bin_search(vec, vec[ranges / 2], score);
+            test = bin_search(vec, vec[ranges - 1], score);
+        }
+
+        shuffle(vec);
+
+        return score;
+    }
+};
 
 int main(int argc, const char *argv[])
 {
-    int max_range = 30;
-    vector<tuple<uintmax_t, uintmax_t>> calculated_scores(max_range);
+    using type = unsigned int;
+    ComputeUnit<type> unit;
+    int max_range = unit.max_range();
+    vector<tuple<type, type>> calculated_scores(max_range);
 
 #pragma omp parallel for
     for(int x = 0; x < max_range; ++x)
     {
-        uintmax_t compute_value = powl(2, x);
-        uintmax_t score = compute(compute_value);
+        type compute_value = static_cast<type>(powl(2, x));
+        type score = unit.compute(compute_value);
         calculated_scores[x] = std::move(make_tuple(compute_value, score));
     }
 
